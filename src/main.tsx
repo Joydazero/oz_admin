e8b4a27b05e58d463f76958dbae19cd484c237d4
Joydazero <36254421+Joydazero@users.noreply.github.com>
2026-02-19 16:27:28 +0900

    Enhance enableMocking for Vercel and mock conditions
    Refactor enableMocking function to support Vercel deployment with mock usage.

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,26 @@ import { worker } from '@/mocks/browser'
 import './index.css'
 
 async function enableMocking() {
-  if (!import.meta.env.DEV) return
+//   if (!import.meta.env.DEV) return
 
-  if (import.meta.env.VITE_USE_MOCK !== 'true') return
+//   if (import.meta.env.VITE_USE_MOCK !== 'true') return
 
-  return worker.start({ onUnhandledRequest: 'bypass' })
+//   return worker.start({ onUnhandledRequest: 'bypass' })
+// }
+
+
+  // 개발 환경이거나, 혹은 Vercel 배포 환경이지만 MOCK을 명시적으로 사용하겠다고 한 경우
+  const isVercel = window.location.hostname.includes('vercel.app');
+  const useMock = import.meta.env.VITE_USE_MOCK === 'true';
+
+  if (import.meta.env.DEV || (isVercel && useMock)) {
+    return worker.start({ 
+      onUnhandledRequest: 'bypass',
+      serviceWorker: {
+        url: '/mockServiceWorker.js' // 경로를 명시해주는 것이 안전합니다.
+      }
+    });
+  }
 }
 
 enableMocking().then(() => {
